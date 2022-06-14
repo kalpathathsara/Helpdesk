c9c454bc4d5ee7dfb89c58f2986364a02062e3a3
kalpathathsara@gmail.com <kalpathathsara@gmail.com>
2022-06-14 15:00:20 +0530

    Initial commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const app = express();
+
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const userRoute = require("./routes/users");
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
+  console.log("Connected to MongoDB");
+});
+
+//middleware
+app.use(express.json());
+app.use(helmet());
+app.use(morgan("common"));
+
+app.use("/api/users", userRoute);
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the Homepage");
+});
+
+app.get("/users", (req, res) => {
+  res.send("Welcome to User Page");
+});
+
+app.listen(8800, () => {
+  console.log("Backend server is running!");
+});
