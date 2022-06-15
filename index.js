ec641ee0ca3d149529b7e6496d9ca22d1d99770d
kalpathathsara@gmail.com <kalpathathsara@gmail.com>
2022-06-16 01:29:09 +0530

    Created register a user and login

--- index.js
+++ index.js
@@ -6,12 +6,17 @@ const dotenv = require("dotenv");
 const helmet = require("helmet");
 const morgan = require("morgan");
 const userRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URL, { useNewUrlParser: true }, () => {
-  console.log("Connected to MongoDB");
-});
+mongoose.connect(
+  process.env.MONGO_URL,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log("Connected to MongoDB");
+  }
+);
 
 //middleware
 app.use(express.json());
@@ -19,6 +24,7 @@ app.use(helmet());
 app.use(morgan("common"));
 
 app.use("/api/users", userRoute);
+app.use("/api/auth", authRoute);
 
 app.get("/", (req, res) => {
   res.send("Welcome to the Homepage");
